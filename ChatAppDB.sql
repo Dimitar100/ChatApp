909f391f01733ced0a33ad25ab2a45de2cbb944d
Dimitar100 <stoilovdimitar29@gmail.com>
2024-03-24 09:55:04 +0200

    Created database with tables - Users, Chats, Messages and Users_Chats

--- ChatAppDB.sql
+++ ChatAppDB.sql
@@ -1,3 +1,37 @@
 drop database ChatAppDB;
 create database ChatAppDB;
 use ChatAppDB;
+
+CREATE TABLE Users(
+	ID INT NOT NULL AUTO_INCREMENT,
+	Username VARCHAR(255) NOT NULL,
+    Pass VARCHAR(255) NOT NULL,
+    User_role ENUM('Admin', 'User') NOT NULL,
+    primary key(ID)
+    );
+
+CREATE TABLE Chats(
+	ID INT NOT NULL AUTO_INCREMENT,
+	ChatName VARCHAR(255) NOT NULL,
+    primary key(ID)
+    );
+    
+CREATE TABLE Messages(
+	ID INT NOT NULL AUTO_INCREMENT,
+	Content VARCHAR(1000) NOT NULL,
+    SenderID INT NOT NULL,
+    ChatID INT NOT NULL,
+    Time_of_sending TIMESTAMP,
+    PRIMARY KEY (ID),
+	FOREIGN KEY (SenderID) REFERENCES Users(ID),
+    FOREIGN KEY (ChatID) REFERENCES Chats(ID)
+    );
+    
+CREATE TABLE Users_Chats(
+	ID INT NOT NULL AUTO_INCREMENT,
+    UserID INT NOT NULL,
+    ChatID INT NOT NULL,
+    PRIMARY KEY (ID),
+	FOREIGN KEY (UserID) REFERENCES Users(ID),
+    FOREIGN KEY (ChatID) REFERENCES Chats(ID)
+    );
