c05f2dc09aa97f74422e2750d28df523494374f5
Dimitar100 <stoilovdimitar29@gmail.com>
2024-03-24 11:58:42 +0200

    added Login and Register activities

--- ChatApp/app/src/main/java/com/dimitar/chatapp/MainActivity.kt
+++ ChatApp/app/src/main/java/com/dimitar/chatapp/MainActivity.kt
@@ -1,17 +1,29 @@
 package com.dimitar.chatapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import java.io.DataInputStream
 import java.io.DataOutputStream
 import java.net.Socket
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val loginButton = findViewById<Button>(R.id.Login)
+        loginButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, Login::class.java)
+            startActivity(intent)
+        }
+        val registerButton = findViewById<Button>(R.id.Register)
+        registerButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, RegisterActivity::class.java)
+            startActivity(intent)
+        }
         //Connect to server
         val t = Thread {
             try {
