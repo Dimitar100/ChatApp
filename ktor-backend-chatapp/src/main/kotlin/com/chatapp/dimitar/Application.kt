0853ad0b7977715373bd0dbd960261b972defc25
Dimitar100 <stoilovdimitar29@gmail.com>
2024-04-02 16:35:10 +0300

    working on hashing the users' passwords

--- ktor-backend-chatapp/src/main/kotlin/com/chatapp/dimitar/Application.kt
+++ ktor-backend-chatapp/src/main/kotlin/com/chatapp/dimitar/Application.kt
@@ -1,6 +1,9 @@
 package com.chatapp.dimitar
 
 import com.chatapp.dimitar.plugins.*
+import com.chatapp.dimitar.security.hashing.SHA256HashingService
+import com.chatapp.dimitar.security.token.JwtTokenService
+import com.chatapp.dimitar.security.token.TokenConfig
 import io.ktor.server.application.*
 
 fun main(args: Array<String>) {
@@ -8,9 +11,17 @@ fun main(args: Array<String>) {
 }
 
 fun Application.module() {
+    val tokenService = JwtTokenService()
+    val tokenConfig = TokenConfig(
+        issuer = environment.config.property("jwt.issuer").getString(),
+        audience = environment.config.property("jwt.audience").getString(),
+        expiresIn = 365L * 1000L * 60L * 60L * 24L,
+        secret = System.getenv("JWT_SECRET")
+    )
+    val hashingService = SHA256HashingService()
     configureSockets()
     configureSerialization()
     configureMonitoring()
-    configureSecurity()
+    configureSecurity(tokenConfig)
     configureRouting()
 }
